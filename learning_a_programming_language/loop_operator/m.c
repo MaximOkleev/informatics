2a09e5a1dcd712041b02881768a3fb1031c8a7bb
Maxim <mak.okleev@gmail.com>
2022-09-16 18:59:25 +0300

    first version of task m

---
+++ learning_a_programming_language/loop_operator/m.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int n = 0;
+	int b = 0;
+	int c = 0;
+	scanf("%d", &n);
+	scanf("%d", &b);
+	n--;
+
+	while(n > 0){
+		scanf("%d", &b);
+		if(b == 0) c++;
+		n--;
+	}
+	printf("%d",c);
+	return 0;
+}
