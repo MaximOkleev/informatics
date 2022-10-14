f64f9df3509d267cbfcb53a489821f22d0e9b21d
Maxim <mak.okleev@gmail.com>
2022-10-14 18:47:56 +0300

    second version of task m

--- learning_a_programming_language/loop_operator/m.c
+++ learning_a_programming_language/loop_operator/m.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 
 int main(){
-	int n = 0;
-	int schit = 0;
+	int count_numbers = 0;
+	int current_number = 0;
 	int count_zero = 0;
 	
-	scanf("%d", &n);
-	scanf("%d", &schit);
-	n--;
+	scanf("%d", &count_numbers);
 
-	while(n > 0){
-		scanf("%d", &schit);
-		if(schit == 0)
+	while(count_numbers-- > 0){
+		scanf("%d", &current_number);
+		if(!current_number)
 		{
 		    count_zero++;
 		}
-		n--;
 	}
 	
-	printf("%d",count_zero);
+	printf("%d", count_zero);
 	return 0;
 }
