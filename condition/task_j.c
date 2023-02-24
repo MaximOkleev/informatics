e9034d7bf8d1d079aa56f09fb2481160c5828c07
Maxim <mak.okleev@gmail.com>
2023-02-24 12:52:11 +0300

    solve condition/task_j

---
+++ condition/task_j.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+    if (abs(c - a) == 1 || abs(d - b) == 1)
+        printf("YES");
+    else
+        printf("NO");
+    return 0;
+}
+
