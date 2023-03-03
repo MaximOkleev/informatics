0bef8f0650038637c19d68bb7621187281778bc8
Maxim <mak.okleev@gmail.com>
2023-03-03 18:10:07 +0300

    task_k/loop_operator

---
+++ task_k.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d;
+    scanf("%d %d %d %d", &a , &b , &c , &d);
+    if(a+2==c&&b+1==d||a+1==c&&b+2==d||a-1==c&&b+2==d||a-2==c&&b+1==d||a-2==c&&b-1==d||a-1==c&&b-2==d||a+1==c&&b-2==d||a+2==c&&b-1==d)printf("YES");
+    else printf("NO");
+    return 0;
+}
